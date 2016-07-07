c7ee4e3c6077fadaa8d7ef54e846a9709a23ff6c
Nima M.Farahani <nima_f101@yahoo.com>
2016-07-06 21:08:03 -0400

    adding methods
    methods added

--- src/convertor/convertor.java
+++ src/convertor/convertor.java
@@ -1,14 +1,156 @@
 package convertor;
+import java.math.BigInteger;
+import java.util.*;
 
 public class convertor {
 
 	public static void main(String[] args) {
 
 		
+		System.out.println("Type in your number:");
+
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		String bigInteger = scan.nextLine();
+		//System.out.println(bigInteger);
+		System.out.println("Pick what type of conversion you want to do by choosing the number given\n"
+				+ "1. Decimal to Binary\n"
+				+ "2. Binary to Decimal\n"
+				+ "3. Decimal to type A\n"
+				+ "4. Type A to Decimal\n"
+				+ "5. Binary to type A\n"
+				+ "6. Type A to Binary\n"
+				+ "7. Decimal to type B\n"
+				+ "8. Type B to Decimal\n"
+				+ "9. Binary to type B\n"
+				+ "10. Type B to Binary\n"
+				+ "11. Type A to type B\n"
+				+ "12. Type B to type A\n"
+				+ "13. convert to all");
+		int option = scan.nextInt();
 		
+		switch(option){
+			
+			case 1: decToBin(bigInteger);
+			case 2: binToDec(bigInteger);
+			case 3: decToA(bigInteger);
+			case 4: aToDec(bigInteger);
+			case 5: binToA(bigInteger);
+			case 6: aToBin(bigInteger);
+			case 7: decToB(bigInteger);
+			case 8: bToDec(bigInteger);
+			case 9: binToB(bigInteger);
+			case 10: bToBin(bigInteger);
+			case 11: aToB(bigInteger);
+			case 12: bToA(bigInteger);
+			case 13: convertToAll(bigInteger);
+			default: System.out.println("wrong choice!");
+		
+		}
 		
 		
 		
 	}
 
+	
+	//1
+	private static String decToBin(String temp){
+		
+		String bin = new BigInteger(temp).toString(2);
+		//System.out.println(bin);		
+		return bin;
+	}
+	
+	//2
+	private static String binToDec(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	
+	//3
+	private static String decToA(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	
+	//4
+	private static String aToDec(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	
+	//5
+	private static String binToA(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	
+	//6
+	private static String aToBin(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	
+	//7
+	private static String decToB(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	
+	//8
+	private static String bToDec(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	//9
+	private static String binToB(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	//10
+	private static String bToBin(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	//11
+	private static String aToB(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	//12
+	private static String bToA(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	//13
+	private static String convertToAll(String temp){
+		String number= null;
+		
+		return number;
+		
+	}
+	
+	
 }
